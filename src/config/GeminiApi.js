6628ad89676dd0b107e1a5f2edcc6a40333de2e1
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-03-23 17:49:27 +0500

    Update GeminiApi.js

--- src/config/GeminiApi.js
+++ src/config/GeminiApi.js
@@ -48,7 +48,6 @@ async function runChat(prompt) {
 
   const result = await chat.sendMessage(prompt);
   const response = result.response;
-  console.log(response.text());
   return response.text();
 }
 
